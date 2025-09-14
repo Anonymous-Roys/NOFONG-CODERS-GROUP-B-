21454cfc26967b79ae50e1739891992575a60fe0
Arhin David Kwabena <davidarhin2005@gmail.com>
2025-09-14 22:50:28 +0000

    open to everyone

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://nofong-coders-group-b.onrender.com'],
+    origin: ['*'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
