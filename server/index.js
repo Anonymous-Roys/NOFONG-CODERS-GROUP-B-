6e0a5f5a42f6b57b21cd9eb6d91f552230e8c141
Arhin David Kwabena <davidarhin2005@gmail.com>
2025-09-14 22:58:48 +0000

    open to everyone8

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "*",
-    credentials: true,
+    // credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
   })
